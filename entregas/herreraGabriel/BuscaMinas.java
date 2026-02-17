262c986ba0f7bdb559754d9e0758689199428160
Gabriel19-87 <gabriel.herrera@alumnos.uneatlantico.es>
2026-02-17 13:31:19 +0100

    refactor: dejar un codigo limpio para que sea facil de leer y comprender su funcionamiento

--- entregas/herreraGabriel/BuscaMinas.java
+++ entregas/herreraGabriel/BuscaMinas.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
 
-class ExamenMina {
+class BuscaMinas {
 
     public static void main(String[] args) {
+
         String[][] mapaMostrar = obtenerMapaMostrar();
         String[][] mapaActivas = obtenerMapaActivas();
+        colocarMinas(mapaActivas);
+
         final int MAX_INTENTOS = 31;
         final int LIMITE_MINAS = 2;
         
@@ -38,63 +41,66 @@ class ExamenMina {
             }
 
         } while (seguirJugando);
-        }
     }
+
     static String[][] obtenerMapaMostrar() {
         return new String[][] {
-            {" ", "1", "2", "3", "4", "5", "6", "7"},
-            {"1", "-", "-", "-", "-", "-", "-", "-"},
-            {"2", "-", "-", "-", "-", "-", "-", "-"},
-            {"3", "-", "-", "-", "-", "-", "-", "-"},
-            {"4", "-", "-", "-", "-", "-", "-", "-"},
-            {"5", "-", "-", "-", "-", "-", "-", "-"}
+                {" ", "1", "2", "3", "4", "5", "6", "7"},
+                {"1", "-", "-", "-", "-", "-", "-", "-"},
+                {"2", "-", "-", "-", "-", "-", "-", "-"},
+                {"3", "-", "-", "-", "-", "-", "-", "-"},
+                {"4", "-", "-", "-", "-", "-", "-", "-"},
+                {"5", "-", "-", "-", "-", "-", "-", "-"}
         };
     }
+
     static String[][] obtenerMapaActivas() {
         return new String[][] {
-            {" ", "1", "2", "3", "4", "5", "6", "7"},
-            {"1", "0", "0", "0", "0", "0", "0", "0"},
-            {"2", "0", "0", "0", "0", "0", "0", "0"},
-            {"3", "0", "0", "0", "0", "0", "0", "0"},
-            {"4", "0", "0", "0", "0", "0", "0", "0"},
-            {"5", "0", "0", "0", "0", "0", "0", "0"}
+                {" ", "1", "2", "3", "4", "5", "6", "7"},
+                {"1", "0", "0", "0", "0", "0", "0", "0"},
+                {"2", "0", "0", "0", "0", "0", "0", "0"},
+                {"3", "0", "0", "0", "0", "0", "0", "0"},
+                {"4", "0", "0", "0", "0", "0", "0", "0"},
+                {"5", "0", "0", "0", "0", "0", "0", "0"}
         };
     }
+
     static void colocarMinas(String[][] mapaActivas) {
-        int i = 0;
+        int colocadas = 0;
         final int TOTAL_MINAS = 5;
-        while (i < TOTAL_MINAS) {
+        while (colocadas < TOTAL_MINAS) {
             int posYMina = (int) (Math.random() * 2 + 1);
             int posXMina = (int) (Math.random() * 4 + 1);
-            if (mapaActivas[posXMina + 1][posYMina + 1].equals("1")) {
-                i--;
-            } else {
+            if (!mapaActivas[posXMina + 1][posYMina + 1].equals("1")) {
                 mapaActivas[posXMina + 1][posYMina + 1] = "1";
+                colocadas++;
             }
-            i++;
         }
     }
-    static void imprimir(String[][] mapa) {
-        for (int i = 0; i < mapa.length; i++) {
-            for (int j = 0; j < mapa[i].length; j++) {
-                System.out.print(mapa[i][j]);
+
+    static void imprimir(String[][] escenario) {
+        for (int i = 0; i < escenario.length; i++) {
+            for (int j = 0; j < escenario[i].length; j++) {
+                System.out.print(escenario[i][j]);
             }
             System.out.println(" ");
         }
     }
+
     static int[] elegirPosicion() {
         Scanner scan = new Scanner(System.in);
-        int x, y;
-        boolean valoresCorrectos = false;
+        int posX, posY;
+        boolean valido = false;
         do {
             System.out.println(" ");
             System.out.println("Ingrese X");
-            y = scan.nextInt();
+            posY = scan.nextInt();
             System.out.println("Ingrese Y");
-            x = scan.nextInt();
-            if (x <= 5 && y <= 7) {
-                valoresCorrectos = true;
+            posX = scan.nextInt();
+            if (posX <= 5 && posY <= 7) {
+                valido = true;
             }
-        } while (!valoresCorrectos);
-        return new int[] { x, y };
+        } while (!valido);
+        return new int[] { posX, posY };
     }
+}
