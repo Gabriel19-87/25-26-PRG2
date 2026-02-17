70914219e42fcb19fe4b348ccdcadb7b91018b10
Gabriel19-87 <gabriel.herrera@alumnos.uneatlantico.es>
2026-02-17 13:28:42 +0100

    refactor: aplicar para el main principal: Se unifican todos los cambios, se renombran variables según el estilo (ej. juegoEncendido a seguirJugando), se usan constantes para límites y se asegura un main corto.

--- entregas/herreraGabriel/BuscaMinas.java
+++ entregas/herreraGabriel/BuscaMinas.java
@@ -5,8 +5,41 @@ class ExamenMina {
     public static void main(String[] args) {
         String[][] mapaMostrar = obtenerMapaMostrar();
         String[][] mapaActivas = obtenerMapaActivas();
-    }
+        final int MAX_INTENTOS = 31;
+        final int LIMITE_MINAS = 2;
+        
+        int contadorMinas = 0;
+        int contadorTurnos = 0;
+        boolean seguirJugando = true;
 
+        do {
+            imprimir(mapaMostrar);
+            int[] posicion = elegirPosicion();
+            int x = posicion[0];
+            int y = posicion[1];
+
+            if (mapaActivas[x][y].equals("1")) {
+                mapaMostrar[x][y] = "x";
+                contadorMinas++;
+            } else if (mapaActivas[x][y].equals("0")) {
+                mapaMostrar[x][y] = ".";
+            } else {
+                System.out.println("No es correcta esa opcion.");
+            }
+
+            contadorTurnos++;
+
+            if (contadorMinas > LIMITE_MINAS) {
+                System.out.println("Has perdido");
+                seguirJugando = false;
+            } else if (contadorTurnos >= MAX_INTENTOS) {
+                System.out.println("Felicidades Ganador!");
+                seguirJugando = false;
+            }
+
+        } while (seguirJugando);
+        }
+    }
     static String[][] obtenerMapaMostrar() {
         return new String[][] {
             {" ", "1", "2", "3", "4", "5", "6", "7"},
@@ -17,7 +50,6 @@ class ExamenMina {
             {"5", "-", "-", "-", "-", "-", "-", "-"}
         };
     }
-
     static String[][] obtenerMapaActivas() {
         return new String[][] {
             {" ", "1", "2", "3", "4", "5", "6", "7"},
@@ -28,7 +60,6 @@ class ExamenMina {
             {"5", "0", "0", "0", "0", "0", "0", "0"}
         };
     }
-}
     static void colocarMinas(String[][] mapaActivas) {
         int i = 0;
         final int TOTAL_MINAS = 5;
@@ -51,7 +82,6 @@ class ExamenMina {
             System.out.println(" ");
         }
     }
-
     static int[] elegirPosicion() {
         Scanner scan = new Scanner(System.in);
         int x, y;
@@ -68,75 +98,3 @@ class ExamenMina {
         } while (!valoresCorrectos);
         return new int[] { x, y };
     }
-	//Posiciones Random de la mina
-	public static void main(String[] args){	
-	Scanner scan = new Scanner(System.in);
-		///////////////////////////
-		int i=0;	
-		while ( i < 5){
-			int PosYMina = (int)(Math.random()*2+1);
-			int PosXMina = (int)(Math.random()*4+1);
-			if (mapaMinasActivas[PosXMina][PosYMina]=="1"){
-			i=i-1;
-			}
-			else {
-				mapaMinasActivas[PosXMina+1][PosYMina+1]="1";
-			}
-			i++;
-		}
-	
-		int posX=0, posY=0, juegoEncendido=1, valoresCorrectos, contadorMapa=0, contadorMinas=0;
-		/////////////Comienza el juego
-		while(juegoEncendido==1){
-			for( i = 0; i<mapaMinasMostrar.length; i++){            
-				for (int j = 0; j<=mapaMinasMostrar.length+1; j++) {                                     
-					System.out.print(mapaMinasMostrar[i][j]);
-				}
-				System.out.println(" ");
-			}
-			valoresCorrectos=0;//reseteo de la variable
-			while(valoresCorrectos==0){
-				System.out.println(" ");
-				System.out.println("Ingrese X");
-				posY = scan.nextInt();
-				System.out.println("Ingrese Y");
-				posX = scan.nextInt();
-				if(posX>5){
-					valoresCorrectos=0;
-				}
-				else if(posY>7){
-					valoresCorrectos=0;
-				}
-				else{
-					valoresCorrectos=1;
-				}
-			}
-
-			if(mapaMinasActivas[posX][posY]=="1"){
-			mapaMinasMostrar[posX][posY]="x";
-			contadorMinas++;
-			}
-			else if(mapaMinasActivas[posX][posY]=="0"){
-			mapaMinasMostrar[posX][posY]=".";
-			}
-			else{
-			System.out.println("No es correcta esa opcion.");
-			}
-			
-			contadorMapa++;
-			if(contadorMinas>2){
-				juegoEncendido=0;
-				System.out.println("Has perdido");
-			}
-			else if(contadorMapa>=31){
-				System.out.println("Felicidades Ganador!");
-				juegoEncendido=0;
-			}
-			else{
-				
-			}
-		}
-	}	
-		
-		
-}
